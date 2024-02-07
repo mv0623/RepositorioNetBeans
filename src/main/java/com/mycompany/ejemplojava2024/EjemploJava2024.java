8dd7240afe2237ea689d9f26271c4032a024b4c0
mv0623 <156902469+mv0623@users.noreply.github.com>
2024-02-07 10:16:21 +0100

    Update EjemploJava2024.java

--- src/main/java/com/mycompany/ejemplojava2024/EjemploJava2024.java
+++ src/main/java/com/mycompany/ejemplojava2024/EjemploJava2024.java
@@ -11,6 +11,6 @@ package com.mycompany.ejemplojava2024;
 public class EjemploJava2024 {
 
     public static void main(String[] args) {
-        System.out.println(">Epi: ¡Hola Blas!");
+        System.out.println((">Epi: ¡Hola Blas!\n>Blas: ¡Hola Epi!\n");
     }
 }
